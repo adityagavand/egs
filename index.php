c2baf114a89c4d210876f46db699cfe80bf40c8e
adityagavand <adityagavand@gmail.com>
2020-09-04 14:22:46 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login form bootstrap</title>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css'><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+           
+            <div class="account-wall">
+                <img class="profile-img" src="mpi.jpeg"
+                    alt="">
+                <form class="form-signin">
+                <input type="text" class="form-control" placeholder="Email" required autofocus>
+                <input type="password" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Sign in</button>
+                
+                <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+
+                <a href="signup.php" class="text-center new-account">Create an account </a>
+                </form>
+            </div>
+            
+        </div>
+    </div>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login form bootstrap</title>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css'><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+           
+            <div class="account-wall">
+                
+                <form class="form-signin">
+                <input type="text" class="form-control" placeholder="Name" required>
+                <input type="text" class="form-control" placeholder="Email" required autofocus>
+                <input type="password" class="form-control" placeholder="Password" required>
+                <input type="password" class="form-control" placeholder="Confirm Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Sign in</button>
+                
+                <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+
+                <a href="#" class="text-center new-account">Create an account </a>
+                </form>
+            </div>
+            
+        </div>
+    </div>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
